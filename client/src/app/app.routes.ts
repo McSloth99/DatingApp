2f76baa2369cedcb764323b9219ff92c9a18bb1c
McSloth99 <mckaylavh@gmail.com>
2024-10-11 10:56:07 +0200

    End Section 7

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { ListsComponent } from './lists/lists.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_gaurds/auth.guard';
+import { TestErrorsComponent } from './error/test-errors/test-errors.component';
+import { NotFoundComponent } from './error/not-found/not-found.component';
+import { ServerErrorComponent } from './error/server-error/server-error.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -17,8 +20,12 @@ export const routes: Routes = [
             {path: 'member/:id', component: MemberDetailComponent},
             {path: 'lists', component: ListsComponent},
             {path: 'messages', component: MessagesComponent},
-            {path: '**', component: HomeComponent, pathMatch: 'full'},         
+                   
         ]
     },
+    {path: 'errors', component: TestErrorsComponent},
+    {path: 'not-found', component: NotFoundComponent},
+    {path: 'server-error', component: ServerErrorComponent},
+    {path: "**", component: HomeComponent, pathMatch: 'full'},
     
 ];
